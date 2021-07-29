43489da942fe8c7a19b599150dd5d8e927b55dfb
adjanybekov <adjanybekov@gmail.com>
2021-07-29 12:08:19 +0600

    commnt

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ var Book = require('./models/book');
 mongoose.connect('mongodb://localhost/bookstore');
 
 var db = mongoose.connection;
-//some comment here
+//some comment here from master
+//some comment here from branch-1
 app.get('/', function (req, res) {
-	res.send('Please use make of /api/student');
+	res.send('Please make use of /api/student');
 });
 
 
